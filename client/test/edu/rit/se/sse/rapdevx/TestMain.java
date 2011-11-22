e0ffbe4beddd7ba432e14d87e546e78ca18a0eb9
Derek Erdmann <derek@derekerdmann.com>
2011-11-21 21:06:45 -0500

    Added sample Java files

---
+++ client/test/edu/rit/se/sse/rapdevx/TestMain.java
@@ -0,0 +1,16 @@
+package edu.rit.se.sse.rapdevx;
+
+import org.junit.Test;
+
+/**
+ * Sample test class
+ * @author Derek Erdmann
+ */
+public class TestMain {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
