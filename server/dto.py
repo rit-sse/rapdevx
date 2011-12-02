524e2c3f46e65b1a464c9dd1b21d5302f5b4d5dd
Brian Besmanoff <keyboarddrummer@gmail.com>
2011-12-02 18:00:34 -0500

    Retabbed and added the AssetImage JSON Encoder

--- server/dto.py
+++ server/dto.py
@@ -2,13 +2,19 @@
 # and the Server Objects. Any class in here is to be exclusively as a struct
 
 class AssetImage:
-	def __init__(self, file, name, registry):
-		self.file = file
-		self.name = name
-		self.id = None #set by registering it
+    def __init__(self, file, name, registry):
+        self.file = file
+        self.name = name
+        self.id = None #set by registering it
+    def jsonEncode(self):
+        r = {}
+        r['file'] = self.file
+        r['name'] = self.name
+        r['id'] = self.id
+        return r
 
 class ShipClass:
-	def __init__(self, types, abilities, maxhp, radius, placement_cost):
+    def __init__(self, types, abilities, maxhp, radius, placement_cost):
         self.types = types
         self.abilities = abilities
         self.maxhp = maxhp
@@ -25,18 +31,18 @@ class Ability:
         self.default_damage = default_damage
         self.special_damages = special_damages
         self.id = None #set by registering it
-		
+        
 class Assets:
     #width, height are ints
     #ship_classes is a list of ShipClass instances
     #images is a list of AssetImage
     #abilities is a list of abilities
-	def __init__(self, width, height, ship_classes, images, abilities):
-		self.width = width
-		self.height = height
-		self.ship_classes = ship_classes
-		self.images = images
-	  
+    def __init__(self, width, height, ship_classes, images, abilities):
+        self.width = width
+        self.height = height
+        self.ship_classes = ship_classes
+        self.images = images
+      
 class ShipPlacement:
     #x,y, classid are all ints
     def __init__(self, x, y, classid):
