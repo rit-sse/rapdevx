1b98bd7356b2a81bd69d6638e8fcf0c87e30339b
Ben Nicholas <ben@ben-nicholas.com>
2011-12-02 14:11:15 -0500

    Bottle super-simple demo

--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
-# Ignore this, just testing
-class Server:
-    
-    def start( self ):
-        return "Started"
+from bottle import route, run
+
+@route('/hello/:name')
+def hello(name='world'):
+	return "Hello " + name + "!"
+
+run(host='localhost', port=8080)
+
