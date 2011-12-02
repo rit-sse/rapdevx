f5f9b1799bba1ecd4a39dca33ad151ea9ed9311e
Ben Nicholas <ben@ben-nicholas.com>
2011-12-02 16:00:02 -0500

    Add POJO data classes

---
+++ client/src/edu/rit/se/sse/rapdevx/api/dataclasses/Session.java
@@ -0,0 +1,12 @@
+package edu.rit.se.sse.rapdevx.api.dataclasses;
+
+/**
+ * POJO representing a Session on the server side of things. Will be 
+ * annotated in order to have Jackson data bindings.
+ *
+ * @author Ben Nicholas
+ */
+public class Session {
+
+}
+
