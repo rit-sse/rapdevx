4b7db422df6565b842f5522d88ef23bed353d4f9
Cody Krieger <cody@codykrieger.com>
2011-12-02 18:43:22 -0500

    Wat, broken package?

--- client/src/edu/rit/se/sse/rapdevx/gui/ImageColorizer.java
+++ client/src/edu/rit/se/sse/rapdevx/gui/ImageColorizer.java
@@ -1,41 +1,44 @@
-package edu.rit.se.sse.rapdevx;
+package edu.rit.se.sse.rapdevx.gui;
 
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class ImageColorizer {
-	protected BufferedImage image;
-	public ImageColorizer(BufferedImage bi){
+	protected BufferedImage	image;
+
+	public ImageColorizer(BufferedImage bi) {
 		image = bi;
 	}
-	
-	public void color(int color, int colorReplaced ){
+
+	public void color(int color, int colorReplaced) {
 		ArrayList<Point> marked = this.findRecolorMarks(colorReplaced);
-		for(int i=0; i<marked.size();i++){
-			image.setRGB((int)marked.get(i).getX(), (int)marked.get(i).getY(), color);
+		for (int i = 0; i < marked.size(); i++) {
+			image.setRGB((int) marked.get(i).getX(),
+					(int) marked.get(i).getY(), color);
 		}
 		image.flush();
 	}
-	
-	public ArrayList<Point> findRecolorMarks(long markColor){
+
+	public ArrayList<Point> findRecolorMarks(long markColor) {
 		ArrayList<Point> markedPoints = new ArrayList<Point>();
 		long rgbValue;
-		for(int i=0;i<image.getWidth();i++){
-			for (int j=0; j<image.getHeight();j++){
-				//System.out.println(Integer.toHexString(0xFFFFFF & image.getRGB(i, j)));
-				rgbValue = image.getRGB(i,j);
+		for (int i = 0; i < image.getWidth(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				// System.out.println(Integer.toHexString(0xFFFFFF &
+				// image.getRGB(i, j)));
+				rgbValue = image.getRGB(i, j);
 				rgbValue &= 0x00FFFFFF;
-				if( rgbValue== markColor){
-					markedPoints.add(new Point(i,j));
-					//System.out.println("Found some magenta");
+				if (rgbValue == markColor) {
+					markedPoints.add(new Point(i, j));
+					// System.out.println("Found some magenta");
 				}
 			}
 		}
 		return markedPoints;
 	}
-	
-	public BufferedImage getImage(){
+
+	public BufferedImage getImage() {
 		return this.image;
 	}
 }
