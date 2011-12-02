290bf8ac6cd3e1a2dd76a62ba636aeb6a1c0c7e8
Mitchell Skiba <git@mskiba.net>
2011-12-02 17:03:30 -0500

    stub stub stub

---
+++ server/gameplay.py
@@ -0,0 +1,27 @@
+class Unit:
+    def __init__(self, abilities, maxhp, owning_player, types):
+        self.id = None #set on registry
+        pass
+        
+    def getMaxHP(self):
+        pass
+        
+    def getAbilities(self):
+        pass
+        
+    def getHP(self):
+        pass
+    
+    def setHP(self):
+        pass
+    
+    def getTypes(self):
+        pass
+        
+    def getPlayer(self):
+        pass
+        
+class Turn:
+    def __init__(self, turn_num):
+        pass
+        
