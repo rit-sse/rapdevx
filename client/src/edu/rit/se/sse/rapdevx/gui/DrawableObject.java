bae143b2e21dbc5efd79f0aeb654f10bf8866ba3
Sean Congden <sean.congden@gmail.com>
2011-12-02 17:57:08 -0500

    Added drawable objects
    Also added a map screen object

---
+++ client/src/edu/rit/se/sse/rapdevx/gui/DrawableObject.java
@@ -0,0 +1,8 @@
+package edu.rit.se.sse.rapdevx.gui;
+
+public abstract class DrawableObject implements Drawable {
+
+	protected int x, y, width, height;
+	protected int xVel, yVel;
+	
+}
---
+++ client/src/edu/rit/se/sse/rapdevx/gui/DrawableShip.java
@@ -0,0 +1,20 @@
+package edu.rit.se.sse.rapdevx.gui;
+
+import java.awt.Graphics2D;
+
+public class DrawableShip extends DrawableObject {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D gPen) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
