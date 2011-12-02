6f7af92d5f9cbb2fd89dc3fdb411dc7a1412cdfc
Brian Besmanoff <keyboarddrummer@gmail.com>
2011-12-02 17:39:02 -0500

    Added the remainder of the methods to server.py

--- server/server.py
+++ server/server.py
@@ -8,21 +8,58 @@ def g_games():
 def p_sessions():
     return "POST /sessions"
 
-@get('/session/:id')
-def g_sessions(id=None):
-    return "GET /session/" + str(id)
+@get('/session/:session_id')
+def g_sessions(session_id=None):
+    return "GET /session/" + str(session_id)
 
-@delete('/session/:id')
-def d_sessions(id=None):
-    return "DELETE /session/" + str(id)
+@delete('/session/:session_id')
+def d_sessions(i=None):
+    return "DELETE /session/" + str(session_id)
 
-@get('/game/:id/assests')
-def g_game_assests(id=None):
-    return "GET /game/" + str(id) + "/assests"
+@get('/game/:game_id/assests')
+def g_game_assests(game_id=None):
+    return "GET /game/" + str(game_id) + "/assests"
 
-@get('/game/:id')
-def g_game(id=None):
-    return "GET /game/" + str(id)
+@get('/game/:game_id')
+def g_game(game_id=None):
+    return "GET /game/" + str(game_id)
+
+@post('/game/:game_id/turns/:turn_id/moves')
+def p_game_turns_moves(game_id=None, turn_id=None):
+    return "POST /game/" + str(game_id) + "/turns/" + str(turn_id) + "/moves"
+
+@get('/game/:game_id/turns/:turn_id/moves')
+def g_game_turns_moves(game_id=None, turn_id=None):
+    return "GET /game/" + str(game_id) + "/turns/" + str(turn_id) + "/moves"
+
+@delete('/game/:game_id/turns/:turn_id/moves/:move_id')
+def d_game_turns_moves(game_id=None, turn_id=None, move_id=None):
+    return "DELETE /game/" + str(game_id) + "/turns/" + str(turn_id) + "/moves/" + str(move_id)
+
+@post('/game/:game_id/turns/:turn_id/attacks')
+def p_game_turns_attacks(game_id=None, turn_id=None):
+    return "POST /game/" + str(game_id) + "/turns/" + str(turn_id) + "/attacks"
+    
+@get('/game/:game_id/turns/:turn_id/attacks')
+def g_game_turns_attacks(game_id=None, turn_id=None):
+    return "GET /game/" + str(game_id) + "/turns/" + str(turn_id) + "/attacks"
+
+
+@delete('/game/:game_id/turns/:turn_id/attacks/:attack_id')
+def d_game_turns_attacks(game_id=None, turn_id=None, attack_id=None):
+    return "DELETE /game/" + str(game_id) + "/turns/" + str(turn_id) + "/attacks/" + attack_id
+
+@post('/game/:game_id/turns/:turn_id/ready')
+def p_game_turns_ready(game_id=None, turn_id=None):
+    return "POST /game/" + str(game_id) + "/turns/" + str(turn_id) + "/ready"
+
+@get('/game/:game_id/turns/:turn_id/moves/results')
+def g_game_turns_moves_results(game_id=None, turn_id=None):
+    return "GET /game" + str(game_id) + "/turns/" + str(turn_id) + "/moves/results"
+
+@get('/game/:game_id/turns/:turn_id/attacks/results')
+def g_game_turns_attacks_results(game_id=None, turn_id=None):
+    return "GET /game" + str(game_id) + "/turns/" + str(turn_id) + "/attacks/results"
 
 run(host='localhost', port=8080)
 
