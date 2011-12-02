4d71296026b861d183a2d6adb42f90d6b28ba320
Brian Besmanoff <keyboarddrummer@gmail.com>
2011-12-02 17:08:39 -0500

    Changed server to respond with the method that you called.

--- server/server.py
+++ server/server.py
@@ -1,8 +1,28 @@
-from bottle import route, run
+from bottle import run, get, post, delete
 
-@route('/hello/:name')
-def hello(name='world'):
-	return "Hello " + name + "!"
+@get('/games')
+def g_games():
+    return "GET /games"
+
+@post('/sessions')
+def p_sessions():
+    return "POST /sessions"
+
+@get('/session/:id')
+def g_sessions(id=None):
+    return "GET /session/" + str(id)
+
+@delete('/session/:id')
+def d_sessions(id=None):
+    return "DELETE /session/" + str(id)
+
+@get('/game/:id/assests')
+def g_game_assests(id=None):
+    return "GET /game/" + str(id) + "/assests"
+
+@get('/game/:id')
+def g_game(id=None):
+    return "GET /game/" + str(id)
 
 run(host='localhost', port=8080)
 
