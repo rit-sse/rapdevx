bfc1f6123abbde1e86c785c3a405c2efbbd75bfb
Brian Besmanoff <keyboarddrummer@gmail.com>
2011-12-02 23:36:34 -0500

    Added the auto path fix to the test file

--- server/test/TestJSON.py
+++ server/test/TestJSON.py
@@ -1,10 +1,18 @@
 import unittest
 import pickle
-from server.dto import *
+import os #path stuff
+import sys #path stuff
+
+#Path Stuff
+path = os.path.abspath(__file__)
+sys.path.append(os.path.join(os.path.dirname(path), '../'))
+
+import dto
+
 class TestJSON(unittest.TestCase):
     def test_AssetImage(self):
-        obj = DTO_AssetImage("aGVsbG8gd29ybGQ=", "ship1", "123")
-        obj1 = JSON_Construct_DTO_AssetImage(obj.encode())
+        obj = dto.DTO_AssetImage("aGVsbG8gd29ybGQ=", "ship1", "123")
+        obj1 = dto.JSON_Construct_DTO_AssetImage(obj.encode())
         self.assertEquals(pickle.dumps(obj),pickle.dumps(obj1))
 
 if __name__ == "__main__":
