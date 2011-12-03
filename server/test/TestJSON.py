058bed67d657f990ed02c6914b66585ce18113d9
Brian Besmanoff <keyboarddrummer@gmail.com>
2011-12-02 23:25:37 -0500

    Fixed the JSON tests to make them run

--- server/test/TestJSON.py
+++ server/test/TestJSON.py
@@ -1,8 +1,12 @@
 import unittest
-
+import pickle
+from server.dto import *
 class TestJSON(unittest.TestCase):
     def test_AssetImage(self):
-        self.assertEquals(1,1)
+        obj = DTO_AssetImage("aGVsbG8gd29ybGQ=", "ship1", "123")
+        obj1 = JSON_Construct_DTO_AssetImage(obj.encode())
+        self.assertEquals(pickle.dumps(obj),pickle.dumps(obj1))
 
 if __name__ == "__main__":
     unittest.main()
+
