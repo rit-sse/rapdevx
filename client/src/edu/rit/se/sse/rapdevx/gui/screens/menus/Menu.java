53d2475e95a8cac3ae49f4e15542f749c8730789
doug <doug@printer-g592t.(none)>
2011-12-02 19:48:09 -0500

    Have Menus and MenuButtons displayable. MenuButtons are added to Menus. Nothing is interactive yet. Screen adjusted to allow expanding or shrinking it's size.

---
+++ client/src/edu/rit/se/sse/rapdevx/gui/screens/menus/Menu.java
@@ -0,0 +1,146 @@
+/**
+ * 
+ */
+package edu.rit.se.sse.rapdevx.gui.screens.menus;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import edu.rit.se.sse.rapdevx.gui.Screen;
+
+
+/**
+ * @author dpk3062
+ */
+public class Menu extends Screen {
+	
+	private final Color BackgroundColor = Color.gray.darker().darker();
+	
+	//Holds all of this menu's buttons
+	private final Collection<MenuButton> buttons = new ArrayList<MenuButton>();
+	
+	//spacing vars
+	private static final int Border = 5;	//space between menu and button edges
+	private static final int Spacing = 5;	//space between buttons
+	private static final int Scale = 1;
+	
+	private static final int EmptyWidth = Border + Border;
+	private static final int EmptyHeight = Border;
+	
+	//true if this menu shouldn't be displayed, else false
+	private boolean isHidden = false;
+	
+	//position
+	private int cornerX = 0;
+	private int cornerY = 0;
+	
+	//--------------------------------------------------------------------------
+	// Constructors
+	//--------------------------------------------------------------------------
+	
+	/**
+	 * @param x The x-axis position of this menu's top left corner
+	 * @param y The y-axis position of this menu's top left corner
+	 */
+	public Menu(int x, int y) {
+		super(EmptyWidth, EmptyHeight);
+		this.cornerX = x;
+		this.cornerY = y;
+	}
+	
+	//--------------------------------------------------------------------------
+	// 
+	//--------------------------------------------------------------------------
+	
+	/**
+	 * Adds a button to this menu.  Buttons are displayed in the order they are added
+	 * @param button A non-null button to add.  If the button has already been added, it is not re-added.
+	 * @throws IllegalStateException If the given button is null
+	 */
+	public void addButton(MenuButton button) throws IllegalStateException {
+		if(button == null) {
+			throw new IllegalStateException("Button cannot be null");
+		} else if(buttons.contains(button)) {
+			return;
+		}
+		
+		{	//TODO update Menu screen size based on button, take into account the button's hidden/visible status too?.  This needs to be refactored into draw to take into account buttons that change sizes due to scaling or other reasons
+			//TODO menu should be able to shrink too, right now it can only grow
+			Dimension screen = getSize();
+			
+			//update height
+			int height = button.getSize().height + Spacing;
+			
+			//update width
+			int width = button.getSize().width + Border + Border;
+			if(width <= screen.width) {
+				width = 0;
+			} else {
+				width = width - screen.width;
+			}
+			adjustSize(width, height);
+		}
+		
+		buttons.add(button);
+	}
+	
+	//--------------------------------------------------------------------------
+	// Methods from Screen
+	//--------------------------------------------------------------------------
+	
+	/* (non-Javadoc)
+	 * @see edu.rit.se.sse.rapdevx.gui.Screen#update(boolean, boolean)
+	 */
+	@Override
+	public void update(boolean hasFocus, boolean isVisible) {
+		//TODO what about hasFocus?
+		this.isHidden = !isVisible;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.rit.se.sse.rapdevx.gui.Screen#updateTransition(double, int)
+	 */
+	@Override
+	public void updateTransition(double position, int direction) {
+		// TODO Auto-generated method stub
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.rit.se.sse.rapdevx.gui.Screen#draw(java.awt.Graphics2D)
+	 */
+	@Override
+	public void draw(Graphics2D gPen) {
+		//if we aren't displayed, just stop
+		if(isHidden) {
+			return;
+		}
+		
+		//draw the backgrond
+		Dimension size = getSize();
+		gPen.setColor(BackgroundColor);
+		gPen.fillRect(cornerX, cornerY, size.width, size.height);
+		
+		//location of where to draw the buttons
+		int x = cornerX + Border;
+		int y = cornerY + Border;
+		int scale = Scale;
+		
+		//draw each button
+		for(MenuButton button: buttons) {
+			//skip hidden buttons
+			if(button.isHidden()) {
+				continue;
+			}
+			
+			button.draw(gPen, x, y, scale);
+			y += button.getSize().height + Spacing;	//moving down for the next button
+			//TODO what about the last button? should it use Spacing or Border?			
+		}
+		
+		//TODO if we don't have focus, draw transparent, light gray myst over ourself
+	}
+	
+}
