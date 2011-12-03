99a4c54296a20e38b16b83a12a055977678eee06
Cody Krieger <cody@codykrieger.com>
2011-12-02 20:59:49 -0500

    AssetLibrary update to accomodate Assets manifest

--- client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
+++ client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
@@ -3,13 +3,32 @@
  */
 package edu.rit.se.sse.rapdevx.clientmodels;
 
+import edu.rit.se.sse.rapdevx.api.dataclasses.Assets;
+
 /**
  * @author Cody Krieger
  * 
  */
 public class AssetLibrary {
-	// private static Assets assets;
+
+	private static Assets	assets;
+
 	// private static Vector<AssetImage> images = new Vector<AssetImage>();
 	// private static Vector<ShipClass> shipClasses = new Vector<ShipClass>();
+
+	/**
+	 * @return the assets
+	 */
+	public static Assets getAssets() {
+		return assets;
+	}
+
+	/**
+	 * @param assets
+	 *            the assets to set
+	 */
+	public static void setAssets(Assets assets) {
+		AssetLibrary.assets = assets;
+	}
 
 }
