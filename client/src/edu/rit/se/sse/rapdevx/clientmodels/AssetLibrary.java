5868a6bea4faa57e0c52d8cd6113a228d5d58ff7
Sean Congden <sean.congden@gmail.com>
2011-12-02 21:16:09 -0500

    Merge?

--- client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
+++ client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
@@ -3,13 +3,43 @@
  */
 package edu.rit.se.sse.rapdevx.clientmodels;
 
+import edu.rit.se.sse.rapdevx.api.dataclasses.Assets;
+
 /**
  * @author Cody Krieger
  * 
  */
 public class AssetLibrary {
-	// private static Assets assets;
-	// private static Vector<AssetImage> images = new Vector<AssetImage>();
-	// private static Vector<ShipClass> shipClasses = new Vector<ShipClass>();
+
+	private static Assets	assets;
+
+	/**
+	 * @return the assets
+	 */
+	public static Assets getAssets() {
+		return assets;
+	}
+
+	/**
+	 * @param assets
+	 *            the assets to set
+	 */
+	public static void setAssets(Assets assets) {
+		AssetLibrary.assets = assets;
+	}
+
+	/**
+	 * @return images for the current game
+	 */
+	public static Object getImages() {
+		return AssetLibrary.assets.getImages();
+	}
+
+	/**
+	 * @return a list of possible ship classes
+	 */
+	public static Object getShipClasses() {
+		return AssetLibrary.assets.getShipClasses();
+	}
 
 }
