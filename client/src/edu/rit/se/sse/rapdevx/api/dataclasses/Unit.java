f7a46f5c6b398372d09a724833201ae920b10df9
Paul Cassidy <illkurok@gmail.com>
2011-12-03 00:07:33 -0500

    finished ability, abilityuseorder,assetimage,movementorder,results,shipclass,shipplacement and renamed ship.java to unit.java

--- client/src/edu/rit/se/sse/rapdevx/api/dataclasses/Unit.java
+++ client/src/edu/rit/se/sse/rapdevx/api/dataclasses/Unit.java
@@ -1,11 +1,11 @@
 package edu.rit.se.sse.rapdevx.api.dataclasses;
 
 /**
- * POJO representing an instance of a ship.
+ * POJO representing an instance of a unit/ship.
  *
  * @author Ben Nicholas
  */
-public class Ship {
+public class Unit {
 
 }
 
