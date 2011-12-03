887cde1cd3440dbc090a9b01abc76060090fc235
Ben Nicholas <ben@ben-nicholas.com>
2011-12-03 17:03:18 -0500

    Properly create games and set player numbers

--- server/gamemanager.py
+++ server/gamemanager.py
@@ -11,12 +11,18 @@ class GameManager:
 
     def create_game(session1=None, session2=None):
         """Creates a new game with the provided sessions connected"""
-        game = GameContext(None)
+        playerlist = [session1.nickname, session2.nickname]
+
+        game = GameContext(playerlist)
         game_id = GameManager.next_game_id()
 
         games[game_id] = game
+        
         session1.game_id = game_id
+        session1.player_num = 1
+        
         session2.game_id = game_id
+        session2.player_num = 2
 
     def find_game(game_id):
         """Finds a game in our database given the game_id."""
