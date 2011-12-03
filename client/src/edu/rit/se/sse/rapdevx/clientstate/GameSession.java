a0ab16bdb73f6bd187180bcb66b7c275914fb275
Cody Krieger <cody@codykrieger.com>
2011-12-02 23:03:19 -0500

    Start communicating with the API during the state initialization

--- client/src/edu/rit/se/sse/rapdevx/clientstate/GameSession.java
+++ client/src/edu/rit/se/sse/rapdevx/clientstate/GameSession.java
@@ -3,18 +3,37 @@
  */
 package edu.rit.se.sse.rapdevx.clientstate;
 
+import edu.rit.se.sse.rapdevx.api.dataclasses.Session;
+
 /**
  * @author Cody Krieger
  * 
  */
 public class GameSession {
 
-	private StateBase	currentState	= new StartingState();
+	private static StateBase	currentState	= new StartingState();
+
+	private static Session		session;
 
 	/**
 	 * Advance the state!
 	 */
-	public void advanceState() {
+	public static void advanceState() {
 		currentState = currentState.advance();
+	}
+
+	/**
+	 * @return the session
+	 */
+	public static Session getSession() {
+		return session;
+	}
+
+	/**
+	 * @param session
+	 *            the session to set
+	 */
+	public static void setSession(Session newSession) {
+		session = newSession;
 	}
 }
