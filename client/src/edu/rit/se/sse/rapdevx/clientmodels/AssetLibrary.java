d8a345c9e4a6c827123585602a1ae95926b45e25
Cody Krieger <cody@codykrieger.com>
2011-12-02 22:26:57 -0500

    Change return types of AssetLibrary

--- client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
+++ client/src/edu/rit/se/sse/rapdevx/clientmodels/AssetLibrary.java
@@ -3,7 +3,11 @@
  */
 package edu.rit.se.sse.rapdevx.clientmodels;
 
+import java.util.List;
+
+import edu.rit.se.sse.rapdevx.api.dataclasses.AssetImage;
 import edu.rit.se.sse.rapdevx.api.dataclasses.Assets;
+import edu.rit.se.sse.rapdevx.api.dataclasses.ShipClass;
 
 /**
  * @author Cody Krieger
@@ -31,15 +35,17 @@ public class AssetLibrary {
 	/**
 	 * @return images for the current game
 	 */
-	public static Object getImages() {
-		return AssetLibrary.assets.getImages();
+	public static List<AssetImage> getImages() {
+		// return AssetLibrary.assets.getImages();
+		return null;
 	}
 
 	/**
 	 * @return a list of possible ship classes
 	 */
-	public static Object getShipClasses() {
-		return AssetLibrary.assets.getShipClasses();
+	public static List<ShipClass> getShipClasses() {
+		// return AssetLibrary.assets.getShipClasses();
+		return null;
 	}
 
 }
