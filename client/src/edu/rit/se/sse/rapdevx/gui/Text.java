9b36a795c41eb9a7aaebb73321bd447bd4eea398
Colin Pitcher <pitcher.colin@gmail.com>
2011-12-03 11:49:29 -0500

    Started the title screen and worked on coloring font.

--- client/src/edu/rit/se/sse/rapdevx/gui/Text.java
+++ client/src/edu/rit/se/sse/rapdevx/gui/Text.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 public class Text implements Drawable{
 	
 	private final File fontFile = new File("assets/FontPage.png");
+	private final File otherFile = new File("assets/FontPageWhite.png");
 	private String toConvert;
 	private int xCord;
 	private int yCord;
@@ -58,6 +59,282 @@ public class Text implements Drawable{
 				try {
 					
 					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 0, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			
+			} else if (character >= 97 && character <= 122) {
+				
+				xIndex = (int) character - 97;
+				xIndex *= 6;
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 12, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character >= 33 && character <= 58) {
+				
+				xIndex = (int) character - 33;
+				xIndex *= 6;
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 24, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character >= 59 && character <= 63) {
+				
+				xIndex = (int) character - 59;
+				xIndex *= 6;
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character >= 91 && character <= 96) {
+				
+				xIndex = (int) character - 91;
+				xIndex *= 6;
+				xIndex += 30;
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character == 124) {
+				
+				xIndex = 66;
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character == 32) {
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(0, 100, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}else {
+				
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(0, 48, 7, 7);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+			gPen.drawImage(smallImage, tempX, yCord, smallImage.getWidth()*this.textScale, 
+					smallImage.getHeight()*this.textScale, null);
+			
+			tempX += 6 * this.textScale;
+			
+		}
+		
+	}
+	
+	public void drawColor(Graphics2D gPen, int toReplace) {
+		
+		int replaceWith = 0x000000;
+		char[] derp = toConvert.toCharArray();
+		int tempX = this.xCord;
+			
+		for ( char character : derp ) {
+				
+			int xIndex;
+			BufferedImage largeImage;
+			BufferedImage smallImage = null;
+			ImageColorizer herp = null;
+				
+			if (character >= 65 && character <= 90) {
+					
+				xIndex = (int) character - 65;
+				xIndex *= 6;
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 0, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			} else if (character >= 97 && character <= 122) {
+					
+				xIndex = (int) character - 97;
+				xIndex *= 6;
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 12, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			} else if (character >= 33 && character <= 58) {
+					
+				xIndex = (int) character - 33;
+				xIndex *= 6;
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 24, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			} else if (character >= 59 && character <= 63) {
+				
+				xIndex = (int) character - 59;
+				xIndex *= 6;
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			} else if (character >= 91 && character <= 96) {
+					
+				xIndex = (int) character - 91;
+				xIndex *= 6;
+				xIndex += 30;
+					
+				try {
+					
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			} else if (character == 124) {
+					
+				xIndex = 66;
+				
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(xIndex, 36, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			} else if (character == 32) {
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(0, 100, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			}else {
+					
+				try {
+						
+					largeImage = ImageIO.read(fontFile);
+					smallImage = largeImage.getSubimage(0, 48, 7, 7);
+					herp = new ImageColorizer(smallImage);
+					herp.colorStrong(replaceWith, toReplace);
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+					
+			}
+				
+			gPen.drawImage(herp.getImage(), tempX, yCord, smallImage.getWidth()*this.textScale, 
+				smallImage.getHeight()*this.textScale, null);
+				
+			tempX += 6 * this.textScale;
+				
+	}
+		
+}
+	
+
+	
+	public void drawWhite(Graphics2D gPen) {
+		
+		char[] derp = toConvert.toCharArray();
+		int tempX = this.xCord;
+		
+		for ( char character : derp ) {
+			
+			int xIndex;
+			BufferedImage largeImage;
+			BufferedImage smallImage = null;
+			
+			if (character >= 65 && character <= 90) {
+				
+				xIndex = (int) character - 65;
+				xIndex *= 6;
+				
+				try {
+					
+					largeImage = ImageIO.read(otherFile);
 					smallImage = largeImage.getSubimage(xIndex, 0, 7, 7);
 					
 				} catch (IOException e) {
