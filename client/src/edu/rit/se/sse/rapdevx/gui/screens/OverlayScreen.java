c3445493e707069a8f48fa9073bcae042d9f3b45
Sean Congden <sean.congden@gmail.com>
2011-12-04 17:24:49 -0500

    Removed audio stuff as its breaking jenkins :(

--- client/src/edu/rit/se/sse/rapdevx/gui/screens/OverlayScreen.java
+++ client/src/edu/rit/se/sse/rapdevx/gui/screens/OverlayScreen.java
@@ -5,8 +5,6 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 
-import edu.rit.se.sse.rapdevx.audio.AudioManager;
-import edu.rit.se.sse.rapdevx.audio.Sound;
 import edu.rit.se.sse.rapdevx.clientstate.AttackState;
 import edu.rit.se.sse.rapdevx.clientstate.DoneState;
 import edu.rit.se.sse.rapdevx.clientstate.GameSession;
@@ -141,7 +139,7 @@ public class OverlayScreen extends Screen implements StateListener
 		{
 			if (undo.containsPoint(e.getPoint()))
 			{
-				AudioManager.get().play(Sound.BattleStart);
+				
 			}
 			else if (redo.containsPoint(e.getPoint()))
 			{
