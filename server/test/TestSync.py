3c0601e879a34ebbbc8a1ee41cf68da4018dba5c
conor <cgw5994@rit.edu>
2011-12-04 15:06:05 -0500

    updated to match DTO changes

--- server/test/TestSync.py
+++ server/test/TestSync.py
@@ -7,44 +7,43 @@ sys.path.append(os.path.join(os.path.dirname(path), '../'))
 
 import dto
 def testAll():
+        encoder=dto.DTO_Encoder()
         f='testSync.txt'
         f=open(f, "w")
         """Asset Image"""
-        aidt=dto.DTO_AssetImage('imgtest.png','testTheGid')
-        f.write('AssetImage:  '+aidt.encode()+'\n')
+        aidt=dto.DTO_AssetImage(b'aimgtest.png','testTheGid')
+        f.write('AssetImage:  '+str(encoder.default(aidt))+'\n')
         """shipclass"""
-        sct=dto.DTO_ShipClass(['this is a gid'], 999, 10, 5, 'imgtest.png','thisisanid')
-        f.write('ShipClass:  '+sct.encode()+'\n')
+        sct=dto.DTO_ShipClass(['goodShip','badShip','SpecialShip','NotSpecialShip'],['this is a gid'], 999, 10, 5, 'imgtest.png','thisisanid')
+        f.write('ShipClass:  '+str(encoder.default(sct))+'\n')
         """ability"""
         at=dto.DTO_Ability(10,'name',5,20,'this is a gid')
-        f.write('Ability:  '+at.encode()+'\n')
+        f.write('Ability:  '+str(encoder.default(at))+'\n')
         """Assets"""
         ast=dto.DTO_Assets(12,53,[sct],[aidt],[at])
-        f.write('Assets:  '+ast.encode()+'\n')
+        f.write('Assets:  '+str(encoder.default(ast))+'\n')
         """ShipPlacement"""
         spt=dto.DTO_ShipPlacement(23,62,'thisisanid')
-        f.write('ShipPlacement:  '+spt.encode()+'\n')
+        f.write('ShipPlacement:  '+str(encoder.default(spt))+'\n')
         """results"""
         rt=dto.DTO_Results({'thisisanid':(23,62)})
-        f.write('Results:  '+rt.encode()+'\n')
+        f.write('Results:  '+str(encoder.default(rt))+'\n')
         """DTO_MovementOrder"""
         mot=dto.DTO_MovementOrder('thisisanid',[(23,62)],'thisisagid')
-        f.write('MovementOrder:  '+mot.encode()+'\n')
+        f.write('MovementOrder:  '+str(encoder.default(mot))+'\n')
         """abilityuseorder"""
         aut=dto.DTO_AbilityUseOrder('srcidtest','targetidtest','this is an ability','gidtest')
-        f.write('AbilityUse:  '+aut.encode()+'\n')
+        f.write('AbilityUse:  '+str(encoder.default(aut))+'\n')
         """attackresults"""
         art=dto.DTO_AttackResults([aut])
-        f.write('AttackResults:  '+art.encode()+'\n')
+        f.write('AttackResults:  '+str(encoder.default(art))+'\n')
         """Unit"""
         ut=dto.DTO_Unit(23,43,'idtest','thisisgid')
-        f.write('Unit:  '+ut.encode()+'\n')
+        f.write('Unit:  '+str(encoder.default(ut))+'\n')
         """status"""
         st=dto.DTO_Status(3,'phase2',['p1','p2'],22)
-        f.write('Status:  '+st.encode()+'\n')
-        """session"""
-        sest=dto.DTO_Session(23, 'cman', 42, True)
-        f.write('Session:  '+sest.encode()+'\n')
+        f.write('Status:  '+str(encoder.default(st))+'\n')
+        f.close()
 
 
 if __name__ == "__main__":
