c2b0814435fb3ada89478c562174c80fb7e16559
Mitchell Skiba <git@mskiba.net>
2011-12-04 18:56:10 -0500

    fixed the DTO_Assets related issues and the test cases

--- server/test/TestJSON.py
+++ server/test/TestJSON.py
@@ -24,10 +24,11 @@ class TestJSON(unittest.TestCase):
         self.assertEqual(pickle.dumps(obj),pickle.dumps(obj1))
     def test_DTO_Assets(self):
         strlist = ["One","Two","Three"]
-        shipclasslist = [dto.DTO_ShipClass(strlist, 5, 5, 5, "Four", "five"), dto.DTO_ShipClass(strlist, 5, 5, 5, "Four", "five"), dto.DTO_ShipClass(strlist, 5, 5, 5, "Four", "five") ] 
-        assetimagelist = [dto.DTO_AssetImage("aGVsbG8gd29ybGQ=", "123"),dto.DTO_AssetImage("aGVsbG8gd29ybGQ=", "123"),dto.DTO_AssetImage("aGVsbG8gd29ybGQ=", "123")]
         abilitylist = [dto.DTO_Ability(5, "One", 5, {"Two":2,"Three":3,"Four":4}, "five"), dto.DTO_Ability(5, "One", 5, {"Two":2,"Three":3,"Four":4}, "five"), dto.DTO_Ability(5, "One", 5, {"Two":2,"Three":3,"Four":4}, "five")]
-        obj = dto.DTO_Assets(5, 5, shipclasslist, assetimagelist, abilitylist)
+        shipclasslist = [dto.DTO_ShipClass(strlist,[abilitylist[0]], 5, 10, 15, "imageid", "gid"),
+                            dto.DTO_ShipClass(strlist,[abilitylist[1]], 6, 11, 14, "imageid2", "gid2")] 
+        assetimagelist = [dto.DTO_AssetImage(b'testbytes', "123"),dto.DTO_AssetImage(b'testbytes2', "123"),dto.DTO_AssetImage(b'testbytes3', "123")]
+        obj = dto.DTO_Assets(5, 6, shipclasslist, assetimagelist, abilitylist)
         obj1 = dto.JSON_Construct_DTO_Assets(obj.toJSON())
         #print("ship_classes0", obj.ship_classes)
         #print("ship_classes1", obj1.ship_classes)
