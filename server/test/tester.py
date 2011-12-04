28a8da420a80170c0d3f30f9fc5f91e562e2c956
unknown <Andrew@morpheus.(none)>
2011-12-04 13:52:18 -0500

    makeing tester.py run tests even if one of the tests fail to import

--- server/test/tester.py
+++ server/test/tester.py
@@ -1,18 +1,12 @@
 import xmlrunner
 import unittest
 
-# Import all tests here
-from TestAttackTurn import *
-from TestClientToServerJSON import *
-from TestGameplay import *
-from TestGamePool import *
-from TestGeometry import *
-from TestJSON import *
-from TestMoveTurn import *
-from TestRegistry import *
-from TestServer import *
-from TestSession import *
-from TestSessionManager import *
+myListOfClasses=["TestAttackTurn","TestJSON", "TestRegistry", "TestGameplay" ,"TestServer"]
+for aClass in myListOfClasses:
+    try:
+        exec("from "+str(aClass)+ " import *")
+    except:
+        print("Can not import "+str(aClass))
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reports/pyunit'))
