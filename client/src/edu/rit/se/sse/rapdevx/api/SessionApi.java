69ced7c28d7a97b3a7c5bda3d285693a89ea9dd2
Cody Krieger <cody@codykrieger.com>
2011-12-04 18:27:25 -0500

    SessionApi functionality fixes

--- client/src/edu/rit/se/sse/rapdevx/api/SessionApi.java
+++ client/src/edu/rit/se/sse/rapdevx/api/SessionApi.java
@@ -10,17 +10,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Map;
 
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.codehaus.jackson.JsonFactory;
@@ -33,68 +27,80 @@ import edu.rit.se.sse.rapdevx.api.dataclasses.Session;
 
 /**
  * API access to the Session object on the server side
- *
+ * 
  * Approximates the ActiveRecord pattern
- *
+ * 
  * @author Ben Nicholas
  */
 public class SessionApi {
-	public static String SERVER_URL = "localhost";
-	
-	private static HttpClient HTTP_CLIENT = new DefaultHttpClient();
-	private static ResponseHandler<String> HANDLER = new BasicResponseHandler();
-	private static ObjectMapper MAPPER = new ObjectMapper(new JsonFactory());
-	private static TypeReference<Map<String, String>> STRING_MAP_TYPE =
-			new TypeReference<Map<String, String>>() { };
-	
+	public static String								SERVER_URL		= "localhost";
+
+	private static HttpClient							HTTP_CLIENT		= new DefaultHttpClient();
+	private static ResponseHandler<String>				HANDLER			= new BasicResponseHandler();
+	private static ObjectMapper							MAPPER			= new ObjectMapper(
+																				new JsonFactory());
+	private static TypeReference<Map<String, String>>	STRING_MAP_TYPE	= new TypeReference<Map<String, String>>() {
+																		};
+
 	/**
-	 * Create a new session, optionally specifying a game to join.  If no game 
-	 * is provided, the session will be registered into the matchmaking pool 
-	 * on the server side and be assigned to a game.
-	 *
-	 * @param gameID The game that the user would like to join.  If 
-	 * there is no preference, pass in null.
-	 *
+	 * Create a new session, optionally specifying a game to join. If no game is
+	 * provided, the session will be registered into the matchmaking pool on the
+	 * server side and be assigned to a game.
+	 * 
+	 * @param gameID
+	 *            The game that the user would like to join. If there is no
+	 *            preference, pass in null.
+	 * 
 	 * @return The initial session object, potentially populated with game_id.
-	 * If no game_id is provided, periodically check for updates until the 
-	 * session has been given a game.
-	 * @throws Exception Talk to cody about this.
+	 *         If no game_id is provided, periodically check for updates until
+	 *         the session has been given a game.
+	 * @throws Exception
+	 *             Talk to cody about this.
 	 */
-	public static Session createSession(String nickname, String gameID) throws Exception {
+	public static Session createSession(String nickname, String gameID)
+			throws Exception {
 		// Connect to session
-		Map<String, String> json = getJsonMap(
-				getResponse("/game/" + gameID));
+		Map<String, String> json = getJsonMap(getResponse("/game/"
+				+ gameID));
 		String incomingJson = "";
 		// pull values
 		json.get("id"); // Don't know what to get
-		
+
 		try {
-			//contents of the POST
-			String data = URLEncoder.encode("nickname", "UTF-8") + "=" + URLEncoder.encode(nickname, "UTF-8");
-		    
-			//if there is a gameID
-			if(gameID != null)data += "&" + URLEncoder.encode("gameID", "UTF-8") + "=" + URLEncoder.encode(gameID, "UTF-8");
-			//url...
+			// contents of the POST
+			String data = URLEncoder.encode("nickname", "UTF-8")
+					+ "="
+					+ URLEncoder.encode(nickname, "UTF-8");
+
+			// if there is a gameID
+			if (gameID != null)
+				data += "&"
+						+ URLEncoder.encode("gameID", "UTF-8")
+						+ "="
+						+ URLEncoder.encode(gameID, "UTF-8");
+			// url...
 			URL url = new URL("http", SERVER_URL, 8080, "/sessions");
-		    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		    conn.setDoOutput(true);
-		    conn.setRequestMethod("POST");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
 
-            conn.connect();
+			conn.connect();
 
-		    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-		    wr.write(data);
-		    wr.flush();
-			
-		 // Get the response
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		    String line;
-		    while ((line = rd.readLine()) != null) {
-		        incomingJson += line;
-		    }
-		    wr.close();
-		    rd.close();
-		    
+			OutputStreamWriter wr = new OutputStreamWriter(
+					conn.getOutputStream());
+			wr.write(data);
+			wr.flush();
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				incomingJson += line;
+			}
+			wr.close();
+			rd.close();
+
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -105,58 +111,69 @@ public class SessionApi {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		if(incomingJson.equals("")){
-			throw new Exception("TALK TO CODY");
-		}else{
-			//need to pull the change for the below method.
+
+		if (incomingJson.equals("")) {
+			throw new Exception("Something is REALLY FUCKED UP.");
+		} else {
+			// need to pull the change for the below method.
 			return Session.fromJSON(incomingJson);
 		}
 	}
 
 	/**
-	 * Check for updates to the given session, primarily used while waiting for 
-	 * a game assignment. 
-	 *
-	 * @param sessionToCheck The session to check for updates(session_id is the 
-	 * only important part)
-	 *
-	 * @return A newly allocated session object, with the server's current view 
-	 * of the provided session.
+	 * Check for updates to the given session, primarily used while waiting for
+	 * a game assignment.
+	 * 
+	 * @param sessionToCheck
+	 *            The session to check for updates(session_id is the only
+	 *            important part)
+	 * 
+	 * @return A newly allocated session object, with the server's current view
+	 *         of the provided session.
 	 */
 	public static Session updateSession(Session sessionToCheck) {
-		String json = getResponse("/session/" + sessionToCheck.getgid());
-		
+		String json = getResponse("/session/"
+				+ sessionToCheck.getgid());
+
 		Session session = Session.fromJSON(json);
-		
+
 		return session;
 	}
 
 	/**
 	 * "Log out" of your current game session.
-	 *
-	 * @param sessionToDestroy The session to be destroyed(session_id is the 
-	 * only important part)
-	 *
-	 * @return A boolean representing whether or not the destroy operation was 
-	 * successful.
+	 * 
+	 * @param sessionToDestroy
+	 *            The session to be destroyed(session_id is the only important
+	 *            part)
+	 * 
+	 * @return A boolean representing whether or not the destroy operation was
+	 *         successful.
 	 */
 	public static boolean destroySession(Session sessionToDestroy) {
 		try {
-			HttpURLConnection con = (HttpURLConnection) (new URL(SERVER_URL).openConnection());
-			con.setDoInput(true);
-			con.setRequestMethod("DELETE");
-			
-		 // Get the response
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		    String line; String incomingJson = "";
-		    while ((line = rd.readLine()) != null) {
-		        incomingJson += line;
-		    }
-		    rd.close();
-		    
-		    if (!incomingJson.equals(""))
-		    	return true;
+			HttpURLConnection conn = (HttpURLConnection) (new URL(SERVER_URL)
+					.openConnection());
+			conn.setDoInput(true);
+			conn.setRequestMethod("DELETE");
+
+			conn.connect();
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String line;
+			String incomingJson = "";
+
+			while ((line = rd.readLine()) != null) {
+				incomingJson += line;
+			}
+
+			rd.close();
+
+			if (!incomingJson.equals(""))
+				return true;
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -164,35 +181,53 @@ public class SessionApi {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Helper method.
 	 * 
 	 * Gets body for request.
 	 * 
-	 * @param args Part after domain (like /game or /session/1)
+	 * @param args
+	 *            Part after domain (like /game or /session/1)
 	 * @return
 	 */
 	private static String getResponse(String args) {
-		HttpGet get = new HttpGet(SERVER_URL + args);
-		String response;
-		
 		try {
-			return HTTP_CLIENT.execute(get, HANDLER);
-		} catch (ClientProtocolException e) {
+			HttpURLConnection conn = (HttpURLConnection) (new URL(SERVER_URL
+					+ args).openConnection());
+			conn.setDoInput(true);
+			conn.setRequestMethod("GET");
+
+			conn.connect();
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			String line;
+			String incomingJson = "";
+
+			while ((line = rd.readLine()) != null) {
+				incomingJson += line;
+			}
+
+			rd.close();
+
+			return incomingJson;
+
+		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	private static Map<String, String> getJsonMap(String content) {
 		try {
 			return MAPPER.readValue(content, STRING_MAP_TYPE);
@@ -206,11 +241,11 @@ public class SessionApi {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
+
 	public static void main(String[] argv) {
-		//createSession("Derp", null);
+		// createSession("Derp", null);
 	}
 }
