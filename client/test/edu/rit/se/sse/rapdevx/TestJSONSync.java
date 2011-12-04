cbdd78af98a458b87def263081c54f5c1a241374
Cody Krieger <cody@codykrieger.com>
2011-12-04 14:16:38 -0500

    Fix Ability list method

--- client/test/edu/rit/se/sse/rapdevx/TestJSONSync.java
+++ client/test/edu/rit/se/sse/rapdevx/TestJSONSync.java
@@ -5,93 +5,103 @@
 
 package edu.rit.se.sse.rapdevx;
 
-import java.util.ArrayList;  //For use as a List
-import java.util.HashMap;    // For use as a Map
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import edu.rit.se.sse.rapdevx.api.dataclasses.*;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Ability;
+import edu.rit.se.sse.rapdevx.api.dataclasses.AbilityUseOrder;
+import edu.rit.se.sse.rapdevx.api.dataclasses.AssetImage;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Assets;
+import edu.rit.se.sse.rapdevx.api.dataclasses.AttackResults;
+import edu.rit.se.sse.rapdevx.api.dataclasses.MovementOrder;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Results;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Session;
+import edu.rit.se.sse.rapdevx.api.dataclasses.ShipClass;
+import edu.rit.se.sse.rapdevx.api.dataclasses.ShipPlacement;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Status;
+import edu.rit.se.sse.rapdevx.api.dataclasses.Unit;
 
 public class TestJSONSync {
 
 	public static void main(String[] args) {
-		//DTO_Ability:
+		// DTO_Ability:
 		Ability ab = new Ability();
 
 		HashMap<String, Integer> ab_spec_damage = new HashMap<String, Integer>();
 		ab_spec_damage.put("one", 1);
 		ab_spec_damage.put("two", 2);
-		
+
 		ab.setRadius(10);
 		ab.setName("name");
 		ab.setDefault_damage(5);
 		ab.setSpecial_damage(ab_spec_damage);
 		ab.setGid("this is a gid");
-		
-		
-		//DTO_AbilityUsoOrder:
+
+		// DTO_AbilityUsoOrder:
 		AbilityUseOrder auo = new AbilityUseOrder();
 		auo.setSrcID("srcidtest");
 		auo.setTargetID("targetidtest");
 		auo.setAbility("this is an ability");
 		auo.setgID("gidtest");
-		
-		//DTO_AssetImage
+
+		// DTO_AssetImage
 		AssetImage ai = new AssetImage();
 		ai.setFile("imgtest.png");
 		ai.setGid("testTheGid");
 
-		//Moved DTO_Asset to the bottom since it relys on another class
+		// Moved DTO_Asset to the bottom since it relys on another class
 
-		//DTO_AttackResults
+		// DTO_AttackResults
 		AttackResults ar = new AttackResults();
-		
-		//DTO_MovementOrder
+
+		// DTO_MovementOrder
 		MovementOrder mo = new MovementOrder();
-		
-		//DTO_Results
+
+		// DTO_Results
 		Results re = new Results();
-		
-		//DTO_Session
+
+		// DTO_Session
 		Session se = new Session();
-		
-		//DTO_ShipClas:
+
+		// DTO_ShipClas:
 		ShipClass sc = new ShipClass();
-		
-		ArrayList<String> sc_ab_lst = new ArrayList<String>();
-		sc_ab_lst.add("this is a gid");		
-		
-		sc.setAbilities(sc_ab_lst);
+
+		// TODO FIX ME LOL
+		// ArrayList<String> sc_ab_lst = new ArrayList<String>();
+		// sc_ab_lst.add("this is a gid");
+		//
+		// sc.setAbilities(sc_ab_lst);
 		sc.setMaxhp(999);
 		sc.setRadius(10);
 		sc.setPlacement_cost(5);
-		//sc.set
-		
-		//DTO_ShipPlacement
+		// sc.set
+
+		// DTO_ShipPlacement
 		ShipPlacement sp = new ShipPlacement();
-		
-		//DTO_Status
+
+		// DTO_Status
 		Status st = new Status();
-		
-		//DTO_Unit
+
+		// DTO_Unit
 		Unit un = new Unit();
-		
-		//DTO_Assests
+
+		// DTO_Assests
 		Assets as = new Assets();
 		as.setWidth(12);
 		as.setHeight(53);
-		
-		//TODO: HERE
+
+		// TODO: HERE
 		ArrayList<Ability> as_abl_lst = new ArrayList<Ability>();
 		as_abl_lst.add(ab);
-		
+
 		ArrayList<AssetImage> as_ai_lst = new ArrayList<AssetImage>();
 		as_ai_lst.add(ai);
-		
+
 		ArrayList<ShipClass> as_sc_lst = new ArrayList<ShipClass>();
 		as_sc_lst.add(sc);
-		//as.setAb
+		// as.setAb
 
-		
-		//Convert each object to JSON
+		// Convert each object to JSON
 		ab.toJSON(ab);
 		auo.toJSON(auo);
 		ai.toJSON(ai);
@@ -104,7 +114,6 @@ public class TestJSONSync {
 		sp.toJSON(sp);
 		st.toJSON(st);
 		un.toJSON(un);
-		
 
 	}
 
