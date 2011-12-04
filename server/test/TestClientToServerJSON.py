d8a3b4c2c23c7e804b4bcac53b87aaa6b8406057
Brian To <bxt5647@rit.edu>
2011-12-04 18:46:17 -0500

    Changed return True to assertEqual in TestClientToServerJSON.py.

--- server/test/TestClientToServerJSON.py
+++ server/test/TestClientToServerJSON.py
@@ -15,88 +15,87 @@ class TestDecoders(unittest.TestCase):
             ait=i
         f.close()
         aidt=dto.JSON_Construct_DTO_AssetImage(ait)
-        if str(dto.DTO_Encoder().encode(aidt))==ait:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(aidt), ait)
+
     def test_ShipClassDec(self):
         f=open('json_sync/ShipClassFromJava.json')
         for i in f:
             sct=i
         f.close()
         scdt=dto.JSON_Construct_DTO_ShipClass(sct)
-        if str(dto.DTO_Encoder().encode(scdt))==sct:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(scdt), sct)
+
     def test_AbilityDec(self):
         f=open('json_sync/AbilityFromJava.json')
         for i in f:
             at=i
         f.close()
         adt=dto.JSON_Construct_DTO_Ability(at)
-        if str(dto.DTO_Encoder().encode(adt))==at:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(adt), at)
+
     def test_AssetsDec(self):
         f=open('json_sync/AssetsFromJava.json')
         for i in f:
             ast=i
         f.close()
         asdt=dto.JSON_Construct_DTO_Assets(ast)
-        if str(dto.DTO_Encoder().encode(asdt))==ast:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(asdt), ast)
+
     def test_ShipPlacementDec(self):
         f=open('json_sync/ShipPlacementFromJava.json')
         for i in f:
             spt=i
         f.close()
         spdt=dto.JSON_Construct_DTO_ShipPlacement(spt)
-        if str(dto.DTO_Encoder().encode(spdt))==spt:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(spdt), spt)
+
     def test_ResultsDec(self):
         f=open('json_sync/ResultsFromJava.json')
         for i in f:
             rt=i
         f.close()
         rdt=dto.JSON_Construct_DTO_Results(rt)
-        if str(dto.DTO_Encoder().encode(rdt))==rt:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(rdt), rt)
+
     def test_MovementOrderDec(self):
         f=open('json_sync/MovementOrderFromJava.json')
         for i in f:
             mot=i
         f.close()
         modt=dto.JSON_Construct_DTO_MovementOrder(mot)
-        if str(dto.DTO_Encoder().encode(modt))==mot:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(modt), mot)
+
     def test_AbilityUseOrderDec(self):
         f=open('json_sync/AbilityUseOrderFromJava.json')
         for i in f:
             auot=i
         f.close()
         auodt=dto.JSON_Construct_DTO_AbilityUseOrder(auot)
-        if str(dto.DTO_Encoder().encode(auodt))==auot:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(auodt), auot)
+
     def test_AttackResultsDec(self):
         f=open('json_sync/AttackResultsFromJava.json')
         for i in f:
             art=i
         f.close()
         ardt=dto.JSON_Construct_DTO_AttackResults(art)
-        if str(dto.DTO_Encoder().encode(ardt))==art:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(ardt), art)
+
     def test_UnitDec(self):
         f=open('json_sync/UnitFromJava.json')
         for i in f:
             ut=i
         f.close()
         udt=dto.JSON_Construct_DTO_Unit(ut)
-        if str(dto.DTO_Encoder().encode(udt))==ut:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(udt), ut)
+
     def test_StatusDec(self):
         f=open('json_sync/StatusFromJava.json')
         for i in f:
             st=i
         f.close()
         sdt=dto.JSON_Construct_DTO_Status(st)
-        if str(dto.DTO_Encoder().encode(sdt))==st:
-            return True
+        self.assertEqual(dto.DTO_Encoder().encode(sdt), st)
 
 if __name__ == "__main__":
     unittest.main()
