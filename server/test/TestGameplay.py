c8c9595b66341d12addb70a1f606206e5427266c
Matt Mokary <mxm6060@rit.edu>
2011-12-04 18:24:07 -0500

    Added tests for gameplay.py in server/test/TestGameplay.py

--- server/test/TestGameplay.py
+++ server/test/TestGameplay.py
@@ -6,28 +6,46 @@ import sys
 path = os.path.abspath(__file__)
 sys.path.append(os.path.join(os.path.dirname(path), "../"))
 
-import gameplay
+from gameplay import *
+import registry
+
+reg = registry.GameRegistry()
+
+TestUnit1 = Unit(abilities=[Ability()], owning_player=0, location=(0, 0))
+reg.register(TestUnit1)
+TestUnit2 = Unit(abilities=[Ability()], owning_player=1, location=(100, 100))
+reg.register(TestUnit2)
+
+move1 = MoveOrder(TestUnit1.gid, (100, 0))
+move2 = MoveOrder(TestUnit2.gid, (0, 100))
 
 class TestGameplay( unittest.TestCase ):
     def test_swizzle1(self):
         lists = [ [0, 1, 2] , [3, 4, 5] ]
-        swizList = gameplay.swizzle(lists)
+        swizList = swizzle(lists)
         self.assertEqual([0, 3, 1, 4, 2, 5], swizList)
     
     def test_swizzle2(self):
         lists = [ [1, 2, 3, 4] , [] ]
-        swizList = gameplay.swizzle(lists)
+        swizList = swizzle(lists)
         self.assertEqual([1, 2, 3, 4], swizList)
     
     def test_swizzle3(self):
         lists = [ [] , [1, 2, 3, 4] ]
-        swizList = gameplay.swizzle(lists)
+        swizList = swizzle(lists)
         self.assertEqual([1, 2, 3, 4], swizList)
 
     def test_swizzle4(self):
         lists = [ [] , [] ]
-        swizList = gameplay.swizzle(lists)
+        swizList = swizzle(lists)
         self.assertEqual([], swizList)
+
+    def test_MoveTurn_execute(self):
+        turn = Turn(0)
+        turn.move.addMoveOrder(move1, 0, reg)
+        turn.move.addMoveOrder(move2, 1, reg)
+        turn.move.execute(reg)
+        self.assertTrue(TestUnit1.location == (100, 0) and TestUnit2.location == (0, 100))
 
 if __name__ == "__main__":
     unittest.main()
