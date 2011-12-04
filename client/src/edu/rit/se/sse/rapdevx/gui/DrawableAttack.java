fb3b1551a02866c873318ec1824974178d4ae67e
Daniel Wallach <dywallach@dywallach.net>
2011-12-03 20:11:13 -0500

    can no longer snap to self. and no longer draws within self

--- client/src/edu/rit/se/sse/rapdevx/gui/DrawableAttack.java
+++ client/src/edu/rit/se/sse/rapdevx/gui/DrawableAttack.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class DrawableAttack extends DrawableObject
 {
@@ -13,7 +14,7 @@ public class DrawableAttack extends DrawableObject
 	private DrawableShip source;
 
 	private AttackScreen parent;
-	
+
 	private DrawableShip selectedShip;
 
 	public DrawableAttack(DrawableShip source, AttackScreen parent)
@@ -25,32 +26,32 @@ public class DrawableAttack extends DrawableObject
 
 	public void draw(Graphics2D gPen)
 	{
-		if(selectedShip != null)
+		if (selectedShip != null)
 		{
 			setMouseLocation(selectedShip.getCenter());
 		}
-		
-		float[] f1 = {10.0f};
-		gPen.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, f1, 0.0f));
-		
+
+		float[] f1 = { 10.0f };
+		gPen.setStroke(new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+				BasicStroke.JOIN_MITER, 10.0f, f1, 0.0f));
+
 		// Draw a line between the last coordinate and the mouse cursor
 		if (mouseLocation != null && source != null)
 		{
 			Point start = source.getCenter();
 			gPen.setColor(new Color(0, 255, 0));
 
-			/*
-			 * Area outside = new Area(new Rectangle2D.Double(0, 0, getWidth(),
-			 * getHeight()));
-			 * outside.subtract(source.getBounds());
-			 * gPen.setClip(outside);
-			 */
+			Area outside = new Area(new Rectangle2D.Double(0, 0, parent.screenWidth,
+					parent.screenHeight));
+			outside.subtract(new Area(source.getBounds()));
+			gPen.setClip(outside);
 
 			gPen.drawLine(start.x, start.y, mouseLocation.x, mouseLocation.y);
 			drawRecticule(gPen, mouseLocation.x, mouseLocation.y);
 		}
-		
+
 		gPen.setStroke(new BasicStroke());
+		gPen.setClip(null);
 	}
 
 	public void setMouseLocation(Point location)
@@ -71,22 +72,26 @@ public class DrawableAttack extends DrawableObject
 	{
 		selectedShip = null;
 		gPen.setStroke(new BasicStroke(2.0f));
-		
+
+		// check if overlaying ship
 		for (DrawableShip ship : parent.getShipList())
 		{
 			if (new Area(ship.getBounds()).contains(mouseLocation.x,
-					mouseLocation.y))
+					mouseLocation.y) && ship != source)
 			{
 				gPen.setColor(new Color(255, 0, 0));
 				selectedShip = ship;
 			}
 		}
 
+		// draw circle
 		gPen.draw(new Ellipse2D.Double(x - 34, y - 34, 68, 68));
 
+		// draw plus
 		gPen.drawLine(x - 50, y, x + 50, y);
 		gPen.drawLine(x, y - 50, x, y + 50);
-		
+
+		// reset color
 		gPen.setColor(new Color(0, 255, 0));
 	}
 }
