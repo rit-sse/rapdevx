5cc0425c181c4147a69e57c8a547008d95df0923
Derek Erdmann <derek@derekerdmann.com>
2011-12-03 22:35:04 -0500

    Updated path in tester xml output

--- server/test/tester.py
+++ server/test/tester.py
@@ -1,7 +1,7 @@
 import xmlrunner
 import unittest
 
-
+# Import all tests here
 from TestAttackTurn import *
 from TestJSON import *
 from TestRegistry import *
@@ -10,4 +10,4 @@ from TestServer import *
 
 
 if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='../../reports/pyunit'))
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='reports/pyunit'))
